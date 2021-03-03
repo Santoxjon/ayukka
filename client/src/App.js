2ebf6c9ab397f394a13b618aa4db5baea60077dc
Santoxjon <jonsantox@gmail.com>
2021-03-04 00:11:59 +0100

    Client making first contact with the api local/remote

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const API_URL = `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`;
+
+  // function postUser(e) {
+  //   e.preventDefault();
+  //   console.log("Holaaaa?");
+  //   let url = "http://127.0.0.1:3456/users";
+  //   let data = { username, password };
+
+  //   fetch(url, {
+  //     method: 'post',
+  //     headers: {
+  //       'Accept': 'application/json, text/plain, */*',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(data)
+  //   }).then(res => res.json())
+  //     .then(res => console.log(res));
+  // }
+
   useEffect(() => {
-    fetch("http://127.0.0.1:3456/users")
+    console.log(API_URL);
+    fetch(`${API_URL}/users`)
       .then(res => res.json())
       .then(res => console.log(res));
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div>
+        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" name="pass" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={postUser}>Registrar</button>
+      </div> */}
+      <form action={`${API_URL}/users`} method="POST">
+        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" name="pass" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Registrar</button>
+      </form>
     </div>
   );
 }
