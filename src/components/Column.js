afeb08534e8f1779b66b411fc0f92e8ec40bbca9
Santoxjon <jonsantox@gmail.com>
2021-03-06 23:59:42 +0100

    id added to column

--- src/components/Column.js
+++ src/components/Column.js
@@ -2,7 +2,7 @@ function Column(params) {
     let column = params.column;
     let taskList = params.taskList;
     return (
-        <div id={column._id} className="column" style={{ backgroundColor: `rgba(${column.color}, .825)` }} >
+        <div id={column._id} className="column" style={{ backgroundColor: `rgba(${column.color}, .55)` }} >
             <h1 className="col-title">
                 {column.name}
             </h1>
