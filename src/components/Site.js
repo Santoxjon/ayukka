35be1306817883baed2b6438a1b6c0904f11e1a0
Santoxjon <jonsantox@gmail.com>
2021-03-07 00:01:23 +0100

    rel attr added and routes for task creation

--- src/components/Site.js
+++ src/components/Site.js
@@ -9,12 +9,12 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Columns from './Columns';
+import CreateTask from './CreateTask';
 
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link,
     Redirect
 } from "react-router-dom";
 
@@ -29,8 +29,8 @@ export function Header() {
                     <Button variant="outline-light">Buscar <FontAwesomeIcon icon={faSearch} flip="horizontal" /></Button>
                 </Form>
                 <Nav className="w-25">
-                    <Button id="addColumnBtn" className="mr-sm-2 w-50" href="www.google.es" target="_blank">Nueva Columna</Button>
-                    <Button id="addTask" className="mr-sm-2 w-50" href="www.google.es" target="_blank">Nueva Tarea</Button>
+                    <Button id="addColumnBtn" className="mr-sm-2 w-50" href="/columns/create" rel="noreferrer">Nueva Columna</Button>
+                    <Button id="addTask" className="mr-sm-2 w-50" href="/columns/tasks/create" rel="noreferrer">Nueva Tarea</Button>
                 </Nav>
             </Navbar>
         </header>
@@ -45,11 +45,20 @@ export function Main() {
                     <Route exact path="/">
                         <Redirect to="/columns" />
                     </Route>
-                    <Route path="/columns">
+                    <Route exact path="/columns">
                         <Columns />
                     </Route>
                     <Route path="/columns/create">
                         <p>Crear columna</p>
+                    </Route>
+                    <Route exact path="/columns/tasks">
+                        <Redirect to="/columns" />
+                    </Route>
+                    <Route path="/columns/tasks/create">
+                        <CreateTask />
+                    </Route>
+                    <Route path="/columns/tasks/:id">
+                        <p>Detalles yo qué sé tío XDD</p>
                     </Route>
                 </Switch>
             </main>
@@ -59,7 +68,7 @@ export function Main() {
 export function Footer() {
     return (
         <footer>
-            <a href="https://github.com/santoxjon/ayukka">
+            <a href="https://github.com/santoxjon/ayukka" target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faGithub} />
                 Every line of code is available here and free to use
             </a>
