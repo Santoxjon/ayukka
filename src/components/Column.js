a9261cfffa96d956f64cdb5c288aad56852c780e
Santoxjon <jonsantox@gmail.com>
2021-03-06 19:27:30 +0100

    column component

---
+++ src/components/Column.js
@@ -0,0 +1,20 @@
+function Column(params) {
+    let column = params.column;
+    let taskList = params.taskList;
+    return (
+        <div id={column._id} className="column" style={{ backgroundColor: `rgba(${column.color}, .825)` }} >
+            <h1 className="col-title">
+                {column.name}
+            </h1>
+            <h2 className="col-description">
+                {column.description}
+            </h2>
+            <hr />
+            <div className="tasks">
+                {taskList}
+            </div>
+        </div>
+    )
+}
+
+export default Column;
