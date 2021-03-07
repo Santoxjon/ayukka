5b87b4b68be3c55c4400abc5eae91ec57c80ee0b
Santoxjon <jonsantox@gmail.com>
2021-03-07 17:05:02 +0100

    create btn style updated

--- src/components/CreateTaskForm.js
+++ src/components/CreateTaskForm.js
@@ -56,7 +56,7 @@ function CreateTaskForm(params) {
             )
         })
         return (
-             columnList 
+            columnList
         )
     }
 
@@ -96,7 +96,7 @@ function CreateTaskForm(params) {
                 <Form.Check inline name="priority" label="10" type="radio" value="10" required />
             </Form.Group>
             <Button variant="dark" type="reset">Vaciar&nbsp;&nbsp;<FontAwesomeIcon icon={faTrashAlt} /></Button>
-            <Button variant="primary" type="submit" style={{ float: "right" }}>Crear&nbsp;&nbsp;<FontAwesomeIcon icon={faPencilAlt} /></Button>
+            <Button className="createTaskBtn" type="submit" style={{ float: "right" }}>Crear&nbsp;&nbsp;<FontAwesomeIcon icon={faPencilAlt} /></Button>
         </Form>
     )
 }
