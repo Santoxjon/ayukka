d247bf6669eb9c3eb2b57b4da13d3c188fbd267d
Santoxjon <jonsantox@gmail.com>
2021-03-09 13:11:28 +0100

    Select to move task removed

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -5,16 +5,6 @@ const API_URL = `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP
 
 function Task(params) {
 
-    const [columns, setColumns] = useState([])
-
-    useEffect(() => {
-        fetch(`${API_URL}/columns`)
-            .then(res => res.json())
-            .then(data => {
-                setColumns(data);
-            })
-    }, [])
-
     let column = params.column;
     let task = params.task;
 
@@ -22,47 +12,16 @@ function Task(params) {
         window.location = `/columns/${e.target.parentNode.parentNode.id.substr(5)}/tasks/${task.id}`
     }
 
-    function Columns() {
-        // let listColumns = columns.filter(col => col.name !== column.name).map(col => {
-        //     return (
-        //         <option value={col._id} data-colName={col.name}>{col.name}</option>
-        //     )
-        // })
-
-        let listColumns = columns.map(col => {
-            return (
-                <option value={col._id} data-colName={col.name}>{col.name}</option>
-            )
-        })
-
-        return (
-            listColumns
-        )
-    }
-
-    function moveTask(e) {
-        let data = { taskId: task.id, columnId: column._id, newColumnId: e.target.value }
-
-        let fetchData = {
-            method: 'PUT',
-            headers: { 'Content-Type': "application/json" },
-            body: JSON.stringify(data)
-        }
-
-        fetch(`${API_URL}/columns/tasks/move`, fetchData)
-            .then((res) => res.json())
-            .then(res => {
-                // window.location = "/";
-                document.querySelector(`#tasks${e.target.value}`).appendChild(document.querySelector(`#_${task.id}`))
-                column._id = e.target.value;
-                let colColor = e.target.parentNode.parentNode.parentNode.style.backgroundColor.split(',');
-                let rgba = `${colColor[0]},${colColor[1]},${colColor[2]}, .85)`
-                e.target.parentNode.style.backgroundColor = rgba;
-            });
+    function dragStart(e) {
+        e.dataTransfer.setData("Text", e.target.id +"-"+e.target.parentNode.id);
+        let tasks_divs = Array.from(document.getElementsByClassName('tasks'))
+        tasks_divs.forEach(div => {
+            div.style.paddingBottom = "50px";
+        });
     }
 
     return (
-        <div id={`_${task.id}`} className="task" style={{ backgroundColor: `rgba(${column.color},.85)` }}>
+        <div onDragStart={dragStart} draggable id={`_${task.id}`} className="task" style={{ backgroundColor: `rgba(${column.color},.85)` }}>
             <h3 className="task-title" onClick={goToTask}>{task.name}</h3>
             <hr />
             <p className="task-text">
@@ -72,11 +31,6 @@ function Task(params) {
             </p>
             <hr />
             <p className="priority">Prioridad: {task.priority}</p>
-            <hr />
-            <Form.Control key="whatever" as="select" custom style={{ padding: "0 5px" }} onChange={moveTask}>
-                <option hidden>Mover tarea a...</option>
-                <Columns key="111" />
-            </Form.Control>
         </div>
     )
 }
